28d228760cda46a46cb033d8f69e7adb54e26399
dw <gpmato10@gmail.com>
2016-02-07 06:31:03 +0900

    p210 add    RedirectAttributes 클래스를 이용해서 URI 상 숨겨진 데이터로 전달함. redirectAttributes.addFlashAttribute("msg", "success");

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
@@ -28,15 +29,14 @@ public class BoardController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String registPost(BoardVO board, Model model) throws Exception {
+    public String registPost(BoardVO board, RedirectAttributes redirectAttributes) throws Exception {
         log.info("regist post .........");
         log.info(board.toString());
 
         service.regist(board);
 
-        model.addAttribute("result", "success!333");
-
-//        return "/board/success";
+        redirectAttributes.addFlashAttribute("msg", "success");
+        
         return "redirect:/board/listAll";
     }
 
