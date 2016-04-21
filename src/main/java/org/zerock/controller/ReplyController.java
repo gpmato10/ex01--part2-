88ce425b661c069ce1afc1a9180e2911ce1bfccd
dw <gpmato10@gmail.com>
2016-04-21 12:34:22 +0900

    p383 특정게시글의 전체 댓글 목록 구현.

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -2,14 +2,12 @@ package org.zerock.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.domain.ReplyVo;
 import org.zerock.service.ReplyService;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by dw on 2016. 4. 20..
@@ -32,6 +30,20 @@ public class ReplyController {
             e.printStackTrace();
             entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+        return entity;
+    }
+
+    @RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+    public ResponseEntity<List<ReplyVo>> list(@PathVariable("bno") Integer bno) {
+        ResponseEntity<List<ReplyVo>> entity = null;
+
+        try {
+            entity = new ResponseEntity<List<ReplyVo>>(service.listReply(bno), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<List<ReplyVo>>(HttpStatus.OK);
+        }
+
         return entity;
     }
 }
