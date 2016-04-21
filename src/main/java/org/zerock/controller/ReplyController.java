bd4a0919c62d1aa6be4eb03779c920e958cf1eb3
dw <gpmato10@gmail.com>
2016-04-21 15:16:28 +0900

    p385 댓글 수정기능까지 완료. 나같은 경우는 http://localhost:8080/replies/1 주소를 이용하였음.

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -46,4 +46,23 @@ public class ReplyController {
 
         return entity;
     }
+
+    @RequestMapping(value = "/{rno}", method = { RequestMethod.PUT, RequestMethod.PATCH} )
+    public ResponseEntity<String> update(@PathVariable("rno") Integer rno,
+                                         @RequestBody ReplyVo vo) {
+
+        ResponseEntity<String> entity = null;
+
+        try {
+            vo.setRno(rno);
+            service.modifyReply(vo);
+
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
 }
